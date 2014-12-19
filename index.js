cf93713d9aa59822fdfcb7c44a71a46d2fc2da7f
Donald Oellerich <doelleri@gmail.com>
2014-12-19 11:38:59 -0500

    initial rule spec api methods

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 'use strict';
 
 var VALID_YEARS = ['2013', '2014'];
+var VALID_OBJECT_TYPES = ['hmda', 'ts', 'lar'];
 
 var SpecAPI = function() {
     var api = {};
 
     api.getValidYears = function() {
         return VALID_YEARS;
+    };
+
+    api.getValidObjectTypes = function() {
+        return VALID_OBJECT_TYPES;
+    };
+
+    api.getFileSpec = function(year) {
+        try {
+            return require('./' + year + '/data_file_specification.json');
+        } catch (e) {
+            return {};
+        }
+    };
+
+    api.getEdits = function(editType, year, objectType) {
+        try {
+            return require('./' + year + '/' + objectType + '-' + editType + '.json');
+        } catch (e) {
+            return [];
+        }
     };
 
     return api;

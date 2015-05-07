f5db444446dde6663316457128c1aa2b82ed545c
Stephen Kent <stevejameskent1@gmail.com>
2015-05-07 00:39:56 -0400

    Move scripts to new folder, remove agency code from file spec

---
+++ scripts/generateDat.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var fs = require('fs');
+
+var spec = require('./index');
+
+var generateWhiteSpace = function(size) {
+    var out = '';
+
+    for (var i = 0; i < size; i++) {
+        out += ' ';
+    }
+
+    return out;
+}
+
+var createOutputLine = function(line, lineNum, lineSpec) {
+    var outputLine = '';
+    var keys = Object.keys(lineSpec);
+    
+    for (var i = 0; i < keys.length; i++) {
+        if (line[keys[i]] === undefined) {
+            throw new Error('File spec does not match the input file. Missing ' + keys[i]);
+        }
+        if (line[keys[i]].toString().length <= lineSpec[keys[i]].length) {
+            // Test the field to see if it's a number. Right justify with white space padding if it is, left justify otherwise
+            if (isNaN(+line[keys[i]].toString())) {
+                outputLine += (line[keys[i]].toString());
+                outputLine += generateWhiteSpace(lineSpec[keys[i]].length - line[keys[i]].toString().length);
+            } else {
+                outputLine += generateWhiteSpace(lineSpec[keys[i]].length - line[keys[i]].toString().length);
+                outputLine += (line[keys[i]].toString());
+            }
+        } else {
+            console.log('Warning: ' + keys[i].toString() + ' for line ' + lineNum + ' is longer than specified in the file spec. This field will be truncated in the final output');
+            outputLine += line[keys[i]].toString().slice(0, lineSpec[keys[i]].length - 1);
+        }
+    }
+
+    return outputLine + '\n';
+};
+
+var fileSpec;
+if (process.argv.length === 5) {
+    fileSpec = spec.getFileSpec(process.argv[2]);       
+}
+
+var inputFile = JSON.parse(fs.readFileSync(process.argv[3]));
+var outputFile = fs.openSync(process.argv[4], 'w');
+
+// Parse the input and write the output file
+var ts = createOutputLine(inputFile.hmdaFile.transmittalSheet, 1, fileSpec.transmittalSheet);
+fs.writeSync(outputFile, ts);
+
+for (var i = 0; i < inputFile.hmdaFile.loanApplicationRegisters.length; i++) {
+    var currLar = inputFile.hmdaFile.loanApplicationRegisters[i];
+    fs.writeSync(outputFile, createOutputLine(currLar, i+2, fileSpec.loanApplicationRegister));
+}
+
+// Close the file once finished
+fs.closeSync(outputFile);
---
+++ scripts/repairFileSpec.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var spec = require('./index');
+
+var fileSpec;
+if (process.argv.length === 3) {
+    fileSpec = spec.getFileSpec(process.argv[2]);
+}
+
+var repairSection = function(keys, currSpec) {
+    var currField = '';
+    var prevField = '';
+    for (var i = 0; i < keys.length; i++) {
+        currField = currSpec[keys[i]];
+        if (i !== 0) {
+            prevField = currSpec[keys[i-1]];
+            currField.start = (+prevField.end) + 1;
+        } else {
+            currField.start = 1;
+        }
+        currField.end = currField.start + ((+currField.length) - 1);
+
+        currField.start = currField.start.toString();
+        currField.end = currField.end.toString();
+    }
+};
+
+try {
+    // Repair transmittal sheet
+    repairSection(Object.keys(fileSpec.transmittalSheet), fileSpec.transmittalSheet);
+
+    // Repair loan application register
+    repairSection(Object.keys(fileSpec.loanApplicationRegister), fileSpec.loanApplicationRegister);
+
+    console.log(JSON.stringify(fileSpec));
+} catch (err) {
+    console.log(err.message);
+}

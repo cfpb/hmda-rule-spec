586c62a2f5deb116f1ee66d2a3f17721f0a1930e
Stephen Kent <stevejameskent1@gmail.com>
2015-05-18 13:18:03 -0400

    Move lar builders and test data to their own directories

---
+++ scripts/larbuilders/sampleLarBuilder.js
@@ -0,0 +1,13 @@
+// Sample lar builder for buildLargeDat.js
+
+var larBuilder = function(year, lar) {
+    var newLar = {};
+
+    // Builder logic here
+
+    return newLar;
+};
+
+// Example command line usage: 
+// node buildLargeDat.js nprm ./nprm.dat ./nprm-large 1000 ./sampleLarBuilder
+module.exports = larBuilder;

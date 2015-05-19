fc74f235c4e6f7a958f22221085ec423ec95d0c0
Stephen Kent <stevejameskent1@gmail.com>
2015-05-19 10:14:18 -0400

    Fix random lar builder

--- scripts/larbuilders/randomLarBuilder.js
+++ scripts/larbuilders/randomLarBuilder.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var _ = require('lodash');
-var spec = require('../index');
+var spec = require('../../index');
 
 var getRandomInt = function(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
